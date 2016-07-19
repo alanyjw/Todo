6913c510fcf277c3849880298467b18075377404
Alan Yeo <ayeo@pivotal.io>
2016-07-19 18:10:39 +0800

    Fix travis.yml

--- test.sh
+++ test.sh
@@ -2,13 +2,13 @@
 
 SCHEME='Todo'
 PROJECT='Todo.xcodeproj'
-SDK='iphonesimulator'
-DESTINATION='platform=iOS Simulator,name=iPhone 6S,OS=9.3'
+SDK='iphonesimulator9.3'
+DESTINATION='platform=iOS Simulator,OS=9.3'
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
-TEST_CMD="xcodebuild -scheme $SCHEME -project $PROJECT -sdk $SDK -destination '$DESTINATION' build test"
+TEST_CMD="xcodebuild -scheme $SCHEME -project $PROJECT -sdk $SDK build test"
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
   eval "${TEST_CMD} | xcpretty"
