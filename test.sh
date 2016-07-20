81012f943da0c87cf1cdb263d8bf2ecdcad8b64e
Alan Yeo <ayeo@pivotal.io>
2016-07-20 11:57:34 +0800

    Add clean step to fix issues in Travis

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ SCHEME="Todo"
 PROJECT="Todo.xcodeproj"
 TEST_SDK="iphonesimulator9.3"
 
-xcodebuild -scheme $SCHEME -project $PROJECT -destination 'platform=iOS Simulator,name=iPhone 6s,OS=9.3' -sdk $TEST_SDK build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty
+xcodebuild -scheme $SCHEME -project $PROJECT -destination 'platform=iOS Simulator,name=iPhone 6s,OS=9.3' -sdk $TEST_SDK clean build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty
