fe9619d8afd472da81d4a07bc2115aa588e03d6d
Alan Yeo <ayeo@pivotal.io>
2016-07-20 11:37:26 +0800

    Remove UITests, since this is done using calabash

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-SCHEME='Todo'
-PROJECT='Todo.xcodeproj'
-TEST_SDK='iphonesimulator9.3'
+SCHEME="Todo"
+PROJECT="Todo.xcodeproj"
+TEST_SDK="iphonesimulator9.3"
 
-TEST_CMD="xctool -scheme $SCHEME -project $PROJECT test -test-sdk $TEST_SDK CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=NO"
-
-eval "$TEST_CMD"
+xctool -scheme $SCHEME -project $PROJECT -destination 'platform=iOS Simulator,name=iPhone 6s,OS=9.3' test -test-sdk $TEST_SDK CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
